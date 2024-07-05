fb5a450fb0d7da4d7c7d959f9ab7c9d26005f23c
demenko <eugenetutor@gmail.com>
2024-07-05 19:25:43 +0300

    Add project files.

---
+++ MVCPatternF/ContactModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCPatternF
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class ContactModel
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public List<Contact> GetContacts() { return contacts; }
+
+        public void AddContact(Contact contact) {  
+            contacts.Add(contact); 
+        }
+
+        public void RemoveContact(int id)
+        {
+            contacts.RemoveAll(contact => contact.Id == id);
+        }
+    }
+}
---
+++ MVCPatternF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVCPatternF
+{
+    public class ContactController
+    {
+        private ContactModel model;
+        private MainWindow view;
+
+        public ContactController(ContactModel model, MainWindow view)
+        {
+            this.model = model;
+            this.view = view;
+            LoadContacts();
+        }
+
+        public void AddContact(string name, string phoneNumber){
+            var newContact = new Contact()
+            {
+                Id = model.GetContacts().Count + 1,
+                Name = name,
+                PhoneNumber = phoneNumber
+            };
+
+            model.AddContact(newContact);
+            LoadContacts();
+        }
+
+        public void RemoveContact(int id)
+        {
+            model.RemoveContact(id);
+            LoadContacts();
+        }
+
+        private void LoadContacts()
+        {
+            view.ContactListBox.Items.Clear();
+            foreach (var contact in model.GetContacts())
+            {
+                view.ContactListBox.Items.Add($"{contact.Name} - {contact.PhoneNumber}");
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private ContactController controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var model = new ContactModel();
+            controller = new ContactController(model, this);
+        }
+
+        private void AddContact_Click(object sender, RoutedEventArgs e)
+        {
+            controller.AddContact(NameTextBox.Text, PhoneNumberTextBox.Text);
+            NameTextBox.Clear();
+            PhoneNumberTextBox.Clear();
+        }
+
+    }
+}
